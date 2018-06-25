321a8466947d2318cfb25cee3a0645eeb2c74a40
contra <yo@contra.io>
2018-06-25 10:28:47 -0400

    clean

--- dist/index.js
+++ dist/index.js
@@ -2,16 +2,15 @@
 
 exports.__esModule = true;
 
-var _requireDir = require('require-dir');
+var _babelCore = require('babel-core');
 
-var _requireDir2 = _interopRequireDefault(_requireDir);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-exports.default = (config = {}) => {
-  // hack to pass keys way down without using env
-  global.__vandelay_util_config = config;
-  return (0, _requireDir2.default)(__dirname, { recurse: true });
+const babelOptions = {
+  babelrc: true,
+  ast: false,
+  sourceMaps: 'inline',
+  filename: 'user-transform.js'
 };
+
+exports.default = code => (0, _babelCore.transform)(code, babelOptions).code;
 
 module.exports = exports['default'];
