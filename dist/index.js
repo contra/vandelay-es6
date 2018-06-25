d708e273adc249cb677f0bcd234125d31f612c4b
contra <yo@contra.io>
2018-06-25 11:15:10 -0400

    release

--- dist/index.js
+++ dist/index.js
@@ -4,12 +4,14 @@ exports.__esModule = true;
 
 var _babelCore = require('babel-core');
 
-const babelOptions = {
-  babelrc: true,
+var _package = require('../package');
+
+const babelOptions = Object.assign({
+  babelrc: false,
   ast: false,
   sourceMaps: 'inline',
   filename: 'user-transform.js'
-};
+}, _package.babel);
 
 exports.default = code => (0, _babelCore.transform)(code, babelOptions).code;
 
